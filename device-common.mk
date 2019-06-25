728fa347f61e193195c2f4a26dffe2577eba3559
washinson <sanya112001@yandex.ru>
2019-06-26 00:51:31 +0300

    RIL fix

--- device-common.mk
+++ device-common.mk
@@ -1,5 +1,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# It crashes RIL
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
